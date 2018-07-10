71261360a368feaa205ce948f1b524c903b69411
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-10 20:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
+  console.log(`story ${name} story`)
   console.log('Welcome, ${name}.${name} You are number in line.')
   return "Welcome, " +name+ ". You are number " +katzDeliLine.length+ " in line."
 }
