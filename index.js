17c61a95f5d15e2efabf8138daec5be8ace7527c
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-10 20:55:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  
+  if(katzDeliLine.length>0) {
+    return katzDeliLine.shift()
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
 }
